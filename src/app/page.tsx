901e4a9835bf228d3edac1cae3ce5aac809b430d
adamdire <adam.dire@mail.mcgill.ca>
2023-02-01 16:43:44 -0500

    Trying more extension stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import {
   QueryDocumentSnapshot,
   Timestamp,
 } from "firebase/firestore";
-import db from "@/firebase";
+import db from "../firebase";
 
 export default async function Home() {
   const data = await getData();
